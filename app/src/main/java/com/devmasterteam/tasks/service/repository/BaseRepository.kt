8e25d04a6f3da937197fa0a970bb9c5af229002c
gabogsb <gbarcellos1701@gmail.com>
2023-12-22 02:59:12 -0300

    set views to tasks expired and next tasks and add register function

--- app/src/main/java/com/devmasterteam/tasks/service/repository/BaseRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/BaseRepository.kt
@@ -1,6 +1,10 @@
 package com.devmasterteam.tasks.service.repository
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
@@ -29,6 +33,42 @@ open class BaseRepository(val context: Context) {
                 listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
             }
         })
+    }
+
+
+    fun isConnectionAvailable(): Boolean {
+
+        var result = false
+
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+
+
+           val activeNet = cm.activeNetwork ?: return false
+           val netWorkCapabilities = cm.getNetworkCapabilities(activeNet) ?: return false
+
+           result = when {
+               netWorkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+               netWorkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+
+               else -> false
+           }
+
+       } else {
+           if (cm.activeNetworkInfo != null) {
+               result = when(cm.activeNetworkInfo!!.type) {
+                   ConnectivityManager.TYPE_WIFI -> true
+                   ConnectivityManager.TYPE_MOBILE -> true
+                   ConnectivityManager.TYPE_ETHERNET -> true
+
+                   else -> false
+               }
+
+           }
+       }
+           return result
+
     }
 
 }
--- app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
@@ -18,7 +18,23 @@ class PersonRepository(context: Context) : BaseRepository(context) {
     private val remote = RetrofitClient.getService(PersonService::class.java)
 
     fun login(email: String, password: String, listener: APIListener<PersonModel>) {
+        if (!isConnectionAvailable()) {
+            listener.onFailure(context.getString(R.string.ERROR_INTERNET_CONNECTION))
+            return
+        }
         executeCall(remote.login(email, password), listener)
     }
+
+    fun create(name: String,email: String, password: String, listener: APIListener<PersonModel>) {
+        if (!isConnectionAvailable()) {
+            listener.onFailure(context.getString(R.string.ERROR_INTERNET_CONNECTION))
+            return
+        }
+        executeCall(remote.create(name, email, password), listener)
+    }
+
+
+
+
 
 }
